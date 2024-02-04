46cceaee5cefdd7e57aa09766d60b20744cbfb1b
IncludeMathH <798511305@qq.com>
2024-02-05 01:48:26 +0800

    add support for stereo video

---
+++ tools/get_StereoVideo.py
@@ -0,0 +1,21 @@
+from  moviepy.editor import *
+import glob, argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='generate stereo video')
+
+    parser.add_argument('--input', default='video/IGEV', help='the dir of mono video')
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    for left in glob.glob('video/IGEV/*_left_*.mp4'):
+        right = left.replace('_left_', '_right_')
+        clips = [VideoFileClip(left), VideoFileClip(right)]
+        video = clips_array([clips])
+        video.write_videofile(left.replace('_left_', '_'))
+
+if __name__=='__main__':
+    main()
+    
